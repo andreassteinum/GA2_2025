3e16749293086f574fe09bee0ec2333f7446cc9e
johnklima <klima.de.sa@gmail.com>
2025-09-30 10:12:40 +0200

    fixed fishing

--- Assets/Scripts/RopeLength.cs
+++ Assets/Scripts/RopeLength.cs
@@ -5,22 +5,24 @@ public class RopeLength : MonoBehaviour
 {
     public Rope rope;
     public CannonBall ball;
+    public BallGravity ballG;
     public float lengthRate = 4.0f;
+    public Transform target;
     // Update is called once per frame
     void Update()
     {
         if (ball.inAir)
             rope.ropeLength += Time.deltaTime * lengthRate ;
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.tag == "Ground")
+        if(Vector3.Distance(transform.position, target.position) < 0.05f)
         {
             Debug.Log("ball hit");
             ball.inAir = false;
             rope.ropeLength = 2;
+            ballG.enabled = false;
         }
-        
+
+
     }
+
 }
