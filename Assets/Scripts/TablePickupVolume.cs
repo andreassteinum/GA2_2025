2465a5117f68773eefe8ddf71122f8cf24800d88
johnklima <klima.de.sa@gmail.com>
2025-09-30 14:01:04 +0200

    pickup tackle

--- Assets/Scripts/TablePickupVolume.cs
+++ Assets/Scripts/TablePickupVolume.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TablePickupVolume : MonoBehaviour
 {
+
+    public GameObject msgpanel;
+    public Text msg;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,13 +19,19 @@ public class TablePickupVolume : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Player")
         {
             Debug.Log("player in volume");
+            msgpanel.SetActive(true);
+            msg.text = "Pickup the fishing pole with your right hand, pick up some bait with your left.";
 
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        msgpanel.SetActive(false);
+    }
 }
