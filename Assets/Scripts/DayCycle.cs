ab458de675832a794d87262b7e80987e2d1ca559
Eli IMG2 <elieke@outlook.com>
2025-10-06 23:19:09 +0200

    Progress on daycycle
    almost done with the daycycle! More tidying in the hierarchy too.
    Finally figured out how to offset the skydome material, but it flickers? Will investigate it more tomorrow.

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -7,21 +7,36 @@ public class DayCycle : MonoBehaviour
     public float dayDuration = 120f; //The duration of a full day in seconds.
     private float rotationSpeed;
     public Gradient ambientColors;
-    private Light sun;
+    public Component sun;
+    public Component celestialCenter;
+
+    //Offsetting the main texture based on time.
+    //public float daySpeed = 10f;
+    //Renderer rend;
+
 
     void Start()
     {
         //Calculate the rotation speed based on day duration.
         rotationSpeed = 360f / dayDuration;
-        sun = GetComponent<Light>();
+        
+        //rend = GetComponent<Renderer>();
     }
 
     void Update()
     {
-        //Rotating the sun as time passes.
-        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        //Rotating the sun (and the moon) as time passes.
+        sun.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        celestialCenter.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
 
-        //Adjusting ambient lighting. No idea if this actually works, I can't tell; but want to keep it just in case!
+        //Changing the offset of the skydome uvs on x as time passes.
+        //was not quite sure how to do this, reddit pointed me this way
+        //https://docs.unity3d.com/ScriptReference/Material-mainTextureOffset.html
+        //float offset = Time.deltaTime * daySpeed;
+        //rend.material.mainTextureOffset = new Vector2(offset, 0);
+
+        //Adjusting ambient lighting based on time of day. 
+        //Can't tell if it works; but want to keep it just in case!
         float timeFactor = Mathf.InverseLerp(-90, 90, transform.eulerAngles.x);
         RenderSettings.ambientLight = ambientColors.Evaluate(timeFactor);
     }
