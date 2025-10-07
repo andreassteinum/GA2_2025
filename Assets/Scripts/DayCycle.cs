0c5d88577f9ce91d320cfd07e4405f666d997e94
Eli IMG2 <elieke@outlook.com>
2025-10-07 22:28:35 +0200

    daycycle
    the amount of times i have commented out a single line of code and tried it slightly differently today without making much progress is frustrating.

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -11,8 +11,8 @@ public class DayCycle : MonoBehaviour
     public Component celestialCenter;
 
     //Offsetting the main texture based on time.
-    //public float daySpeed = 10f;
-    //Renderer rend;
+    public float daySpeed = 0.06f;
+    Renderer rend;
 
 
     void Start()
@@ -20,7 +20,7 @@ public class DayCycle : MonoBehaviour
         //Calculate the rotation speed based on day duration.
         rotationSpeed = 360f / dayDuration;
         
-        //rend = GetComponent<Renderer>();
+        rend = GetComponent<Renderer>();
     }
 
     void Update()
@@ -29,12 +29,13 @@ public class DayCycle : MonoBehaviour
         sun.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
         celestialCenter.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
 
-        //Changing the offset of the skydome uvs on x as time passes.
+        //Changing the offset of the skydome material on x as time passes.
         //was not quite sure how to do this, reddit pointed me this way
         //https://docs.unity3d.com/ScriptReference/Material-mainTextureOffset.html
-        //float offset = Time.deltaTime * daySpeed;
-        //rend.material.mainTextureOffset = new Vector2(offset, 0);
-
+        float offset = Time.deltaTime * daySpeed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0); 
+        //ugh it works but not in the way i want it to! why is it flickering instead of progressing infinitely?
+       
         //Adjusting ambient lighting based on time of day. 
         //Can't tell if it works; but want to keep it just in case!
         float timeFactor = Mathf.InverseLerp(-90, 90, transform.eulerAngles.x);
